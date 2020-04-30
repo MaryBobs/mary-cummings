63ef2f95f1dd68b40b6428151f5fc347e3160f33
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-04-30 15:28:02 +0100

    adds article and article-list pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,45 +2,19 @@ import React from 'react';
 import {Link} from 'gatsby';
 import Layout from '../components/layout';
 import Title from '../components/title.js';
+import ArticleList from '../components/article-list';
 
-export default ({data}) => (
+export default () => (
     <Layout>
     <Title text="Welcome"/>
-    <div>
-        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
-    </div>
+   
     <p>
         Lorem ipsum dolor sit amet
     </p>
 
-    <div>
-        {data.allMarkdownRemark.edges.map(({node}) => (
-            <article>
-                <div>
-                    <strong>{node.frontmatter.title}</strong>
-                </div>
-                <div>
-                    {node.excerpt}
-                </div>
-            </article>
-        ))}
-    </div>
+    <ArticleList />
+
+  
     </Layout>
 )
 
-export const query = graphql `query {
-    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
-        totalCount
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-              image
-              date(formatString: "DD MMMM YYYY")
-            }
-            excerpt
-          }
-        }
-      }
-}`
