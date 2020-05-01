7a3866239951b645a3af40e456dc7ee4caace7e7
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-05-01 13:17:13 +0100

    adds profile component

---
+++ src/components/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <h2>Hi, I'm Mary</h2>
+        <p>I recently completed a Professional Software Development course at CodeClan here in Edinburgh and am now looking for my first job in the world of tech.</p>
+        <p>However the small matter of a global pandemic has slightly derailed the job hunt!</p>
+        <p>Alongside the job applications I'm trying to keep on learning and teaching myself online. I've built this website as somewhere to hold my new blog and other bits and pieces I'm working on.</p>
+        <p>At the moment it is pretty small but I just wanted to get something published and then keep adding to it, rather than spending ages adding every little detail I want to!</p>
+        <p>If you want to get in touch please do! Links to my LinkedIn and Twitter are at the top of the page.</p>
+    </div>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {Link} from 'gatsby';
 import Layout from '../components/layout.js';
 import Title from '../components/title.js';
+import Profile from '../components/profile.js';
 
 export default () => (
     <Layout>
     <Title text="About Me"/>
-    <div>
-        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
-    </div>
-    <p>
-        Lorem ipsum dolor sit amet
-    </p>
+    <Profile />
     </Layout>
 )
