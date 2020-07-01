e13a048a418feac6429474743bee2c47d586641a
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-07-01 12:10:03 +0100

    moves images folder to static folder

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -4,26 +4,12 @@ import Project from '../components/project';
 
 const projectList = () => (
     <div>
-        <h1>{projectData.heading}</h1>
         {projectData.content.map( (data) => {
-            return <Project key={data.slug} title={data.title}/>
+            return <Project key={data.slug} title={data.title} image={data.image} details={data.details}
+            url={data.url} github={data.github}/>
         })}
     </div>
 )
-
-    // export default (props) => (
-    //     <div>
-    //         {props.projects.map(
-    //         ({project}) => (
-    //             <Project
-    //             name={project.name}
-    //             image={project.image}
-    //             details={project.details}
-    //             url={project.url}
-    //             github={project.github}/>
-    //         ))}
-    //     </div>
-    // )
 
 export default projectList
 
--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default (props) => (
+    <div>
     <h1>{props.title}</h1>
+    <h2>{props.details}</h2>
+    <img src={props.image}></img>
+    <a href={props.github} target="_blank" rel="noopener noreferrer">Github Repo</a>
+    <a href={props.url} target="_blank" rel="noopener noreferrer">Site Demo</a>
+    </div>
 )
